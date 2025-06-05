c4ccd9f97dd058609b9332f6eaa8db61a5058a11
Tibin Sebastian <tibinsebastian21@gmail.com>
2025-06-05 22:54:51 +0530

    hello

---
+++ Basic Calculator/Basic Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyTemplateProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a basic calculator");
+            do
+            {
+                Console.Write("Enter first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter an operator (+, -, *, /): ");
+                char op = Console.ReadKey().KeyChar;
+                switch (op)
+                {
+                    case '+':
+                        Console.WriteLine($"\nResult: {num1 + num2}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"\nResult: {num1 - num2}");
+                        break;
+                    case '*':
+                        Console.WriteLine($"\nResult: {num1 * num2}");
+                        break;
+                    case '/':
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine($"\nResult: {num1 / num2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nError: Division by zero is not allowed.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\nError: Invalid operator. Please use +, -, *, or /.");
+                        break;
+                }
+                Console.Write("Do you want to perform another calculation? (y/n): ");
+                char again = Console.ReadKey().KeyChar;
+                if (again != 'y' && again != 'Y')
+                {
+                    Console.WriteLine("\nExiting the calculator. Goodbye!");
+                    break;
+                }
+            } while (true); 
+        }
+    }
+}
+
