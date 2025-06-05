3962a119ff02206c7c81061be76b0784e6debeb8
Tibin Sebastian <tibinsebastian21@gmail.com>
2025-06-05 22:19:02 +0530

    number guessing game

---
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyTemplateProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Guess a number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+
+            if ( number == randomNumber)
+            {
+                Console.WriteLine("Guess Correct");
+            }
+            else
+            {
+                Console.WriteLine("Guess Incorrect");
+                Console.WriteLine($"The correct number was {randomNumber}");
+            }
+        }
+    }
+}
