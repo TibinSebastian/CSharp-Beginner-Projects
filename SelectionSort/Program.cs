b67bebc4ede021f939ffa8f59d873dfd3af5caaf
Tibin Sebastian <tibinsebastian21@gmail.com>
2025-06-17 20:05:55 +0530

    Selection Sort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 64, 25, 12, 22, 11 };
+        SelectionSort(numbers);
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
